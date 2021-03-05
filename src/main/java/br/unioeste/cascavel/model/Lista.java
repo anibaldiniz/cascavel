9562b2d5d401ea70fbb12e235a94f43af6a64bde
Anibal Mantoani Diniz <anibaldiniz@gmail.com>
2021-03-05 19:56:16 -0300

    atualização do UpdateUser

---
+++ src/main/java/br/unioeste/cascavel/model/Lista.java
@@ -0,0 +1,30 @@
+package br.unioeste.cascavel.model;
+
+
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Data
+@Entity
+public class Categoria {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;   //discente  docente  técnico adminstrativo estagiário
+
+    @ManyToMany(mappedBy = "categorias", cascade = CascadeType.ALL)
+    private List<Usuario> usuarios;
+
+}
